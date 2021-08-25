dfbdeaf44c2181121f1ab47dc806c430e072396f
hihasan <hihasan07@gmail.com>
2021-08-25 11:43:58 +0600

    Search VIew

--- app/src/main/java/com/hihasan/music/views/home/HomeFragment.kt
+++ app/src/main/java/com/hihasan/music/views/home/HomeFragment.kt
@@ -5,10 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.hihasan.music.R
 import com.hihasan.music.databinding.FragmentHomeBinding
 import com.hihasan.music.utils.BaseFragment
 
-class HomeFragment: BaseFragment() {
+class HomeFragment: BaseFragment(), View.OnClickListener {
     var binding:FragmentHomeBinding? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -20,8 +22,21 @@ class HomeFragment: BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.d("Hihasan", "I Came here")
-//        initListeners()
+        initListeners()
 //        setupViewPager()
     }
+
+    private fun initListeners() {
+        binding!!.searchIv.setOnClickListener {
+            findNavController().navigate(R.id.nav_home_to_nav_search)
+        }
+    }
+
+    override fun onClick(view: View?) {
+//        when{
+//
+//        }
+    }
+
 
 }
--- app/src/main/java/com/hihasan/music/views/search/SearchFragment.kt
+++ app/src/main/java/com/hihasan/music/views/search/SearchFragment.kt
@@ -1,6 +1,35 @@
 package com.hihasan.music.views.search
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hihasan.music.databinding.FragmentHomeBinding
+import com.hihasan.music.databinding.FragmentSearchBinding
 import com.hihasan.music.utils.BaseFragment
 
-class SearchFragment: BaseFragment() {
+class SearchFragment: BaseFragment(), View.OnClickListener {
+    var binding: FragmentSearchBinding? =  null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding!!.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("Hihasan", "I Came here")
+//        initListeners()
+//        setupViewPager()
+    }
+
+    private fun initListeners() {
+//        binding!!.s
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
+    }
 }
