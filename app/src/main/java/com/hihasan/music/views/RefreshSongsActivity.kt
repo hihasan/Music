82f5c9d75018006a266f302016a4f3eb3b23a92d
hihasan <hihasan07@gmail.com>
2021-09-10 14:31:10 +0000

    Responsiveness

--- app/src/main/java/com/hihasan/music/views/RefreshSongsActivity.kt
+++ app/src/main/java/com/hihasan/music/views/RefreshSongsActivity.kt
@@ -1,7 +1,25 @@
 package com.hihasan.music.views
 
+import android.os.Bundle
+import com.hihasan.music.databinding.ActivityRefreshBinding
 import com.hihasan.music.utils.BaseActivity
+import com.hihasan.music.utils.Singleton
 
 class RefreshSongsActivity: BaseActivity() {
 
+    private var binding: ActivityRefreshBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRefreshBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        Singleton.getInstance().context = applicationContext
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        TODO("Not yet implemented")
+    }
 }
